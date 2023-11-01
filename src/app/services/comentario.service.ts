6210cbad957b6bc52788459316ea27e1f91f636f
Lucas-Santini <114113893+Lucas-Santini@users.noreply.github.com>
2023-11-01 18:58:45 -0300

    Update comentario.service.ts
    Desenvolvi todas as funções de CRUD do service de chapter assunto COMENTARIO no projeto angular.

--- src/app/services/comentario.service.ts
+++ src/app/services/comentario.service.ts
@@ -4,37 +4,27 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
-const httpOptions = {
-  headers: new HttpHeaders ({
-    'Content-Type' : 'application/json',
-    'Authorization': `Bearer ${localStorage.getItem('TokenUsuarioLogado')}`
-  }),
-};
-
 @Injectable({
   providedIn: 'root'
 })
 export class ComentarioService {
-
   url = environment.apiServer + 'api/ChapterAssuntoComentario';
-  constructor(private https: HttpClient) { }
 
-  ObterTodos(): Observable<ChapterAssuntoComentario[]>{
-    const apiUrl = `${this.url}`;
-    let teste = this.https.get<ChapterAssuntoComentario[]>(apiUrl)
-    return teste;
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('TokenUsuarioLogado')}`
+    })
+  };
+
+  NovoChapterAssuntoComentario(comentario: ChapterAssuntoComentario): Observable<ChapterAssuntoComentario> {
+    return this.http.post<ChapterAssuntoComentario>(this.url, comentario, this.httpOptions);
   }
 
-  FiltrarChapterAssuntoComentarioPorId (id: number) : Observable<ChapterAssuntoComentario[]>
-  {
+  FiltrarChapterAssuntoComentarioPorId(id: number): Observable<ChapterAssuntoComentario[]> {
     const apiUrl = `${this.url}/filterByChapterAssuntoId/${id}`;
-    let teste = this.https.get<ChapterAssuntoComentario[]>(apiUrl)
-    return teste;
-  }
-
-  NovoChapterAssuntoComentario (comentario: ChapterAssuntoComentario): Observable<ChapterAssuntoComentario>
-  {
-    return this.https.post<ChapterAssuntoComentario>(this.url, comentario, httpOptions);
+    return this.http.get<ChapterAssuntoComentario[]>(apiUrl);
   }
 }
